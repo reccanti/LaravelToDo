3183d25bb919b0d7441312525d255814f2350810
Ben Wilcox <reccanti@gmail.com>
2017-03-21 12:06:57 -0400

    Create view for todo list
    Creates a view that will display the items in the todo list

---
+++ resources/views/tasks.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ config('app.locale') }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel To Do List</title>
+
+        <!-- Fonts -->
+
+        <!-- Styles -->
+    </head>
+    <body>
+        <div id="list">
+            To Do list will go here
+        </div>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,9 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/tasks');
+});
+
+Route::get('/tasks', function () {
+    return view('tasks');
 });
